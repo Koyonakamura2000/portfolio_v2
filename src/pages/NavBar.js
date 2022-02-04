f57460ebdcd15e26b9bd58ff8171dd8d90762877
Koyo Nakamura <koyonakamura123@gmail.com>
2022-02-03 18:18:52 -0800

    set up nav bar, reorganized images, icons, pdfs into assets folder

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,11 +1,17 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
+import resumePdf from '../assets/pdfs/01_2022_UX_Resume.pdf';
+import './NavBar.css';
 
 function NavBar() {
     return (
         <>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
+            <nav className='nav-bar'>
+                <Link className='logo' to='/'>Koyo Nakamura</Link>
+                <div className='nav-links'>
+                    <NavLink exact='true' className={(state) => state.isActive ? 'nav-selected' : 'nav-unselected'} to='/'>My Work</NavLink>
+                    <NavLink className={(state) => state.isActive ? 'nav-selected' : 'nav-unselected'} to='/about'>About</NavLink>
+                    <a className='nav-resume-link' href={resumePdf} rel='noreferrer' target='_blank'>Resume</a>
+                </div>
             </nav>
             <Outlet />
         </>
